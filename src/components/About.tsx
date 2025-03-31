022ab10e5aa65b7a90aec35bd857e848ce04dfab
AmitabhRithwan <amit4app@gmail.com>
2025-03-31 22:00:34 +0530

    text changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,7 @@ const About: React.FC = () => {
         <div className="row gy-4">
           <div className="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
             <p>
-              I am a passionate <strong>Mobile App Developer</strong> with <strong>11 years of experience</strong>, specializing in <strong>React Native, Vue, Ionic, and Native Android (Java + Kotlin)</strong>.
+              I am a passionate <strong>Mobile App Developer</strong> with <strong>11 years of experience</strong>, specializing in <strong>React Native, Vue, Ionic and Native Android (Java + Kotlin)</strong>.
             </p>
             <ul>
               <li><i className="bi bi-check2-circle"></i> Expertise in cross-platform and native mobile development.</li>
